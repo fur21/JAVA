3cba17e1b49b26cab5a43e5870c418241dae175b
장찬영 <jang960222@naver.com>
2025-01-09 00:52:08 +0900

    비트 논리 연산자

---
+++ src/Operator/BitLogicExample.java
@@ -0,0 +1,10 @@
+package Operator;
+
+public class BitLogicExample {
+    public static void main(String[] args) {
+        System.out.println("45 & 24 = " + (45 & 25));
+        System.out.println("45 | 24 = " + (45 | 25));
+        System.out.println("45 ^ 24 = " + (45 ^ 25));
+        System.out.println("~45 = " + (~45));
+    }
+}
