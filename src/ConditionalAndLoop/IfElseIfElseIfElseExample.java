7ba8e0e0f7e145a63692dbac2032be413296572f
장찬영 <jang960222@naver.com>
2025-01-09 17:14:28 +0900

    if문, else if문, else문 연습

---
+++ src/ConditionalAndLoop/IfElseIfElseIfElseExample.java
@@ -0,0 +1,18 @@
+package ConditionalAndLoop;
+
+public class IfElseIfElseIfElseExample {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if(score >= 90) {
+            System.out.println("등급은 A입니다");
+        } else if(score >= 80){
+            System.out.println("등급은 B입니다");
+        } else if(score >= 70){
+            System.out.println("등급은 C입니다");
+        } else {
+            System.out.println("등급은 D입니다");
+        }
+
+    }
+}
