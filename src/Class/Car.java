d4fa2c861e6d0be4bacb76fceefb0eee9dcded9e
장찬영 <jang960222@naver.com>
2025-02-26 18:39:12 +0900

    다른 생성자를 호출해서 중복 코드 줄이기 및 객체 생성 후 출력

--- src/Class/Car.java
+++ src/Class/Car.java
@@ -9,7 +9,20 @@ public class Car {
     int speed;
 
     // 생성자
-    Car(String color, int cc) {
+    Car() {}
 
+    Car(String model) {
+        this(model, "은색", 250);
     }
+
+    Car(String model, String color) {
+        this(model, color, 250);
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
 }
--- src/Class/CarExample.java
+++ src/Class/CarExample.java
@@ -3,7 +3,7 @@ package Class;
 public class CarExample {
     public static void main(String[] args) {
         // 객체 생성
-        Car myCar = new Car("검정", 3000);
+        Car myCar = new Car("검정", "3000");
 
         // 필드값 읽기
         System.out.println("제작회사 : " + myCar.company);
@@ -15,5 +15,27 @@ public class CarExample {
         // 필드값 변경
         myCar.speed = 60;
         System.out.println("수정된 속도 : " + myCar.speed);
+
+        Car car1 = new Car();
+        System.out.println("car1.company : " + car1.company);
+        System.out.println("------------------------------");
+
+        Car car2 = new Car("자가용");
+        System.out.println("car2.company : " + car2.company);
+        System.out.println("car2.model : " + car2.model);
+        System.out.println("------------------------------");
+
+        Car car3 = new Car("자가용", "빨강");
+        System.out.println("car3.company : " + car3.company);
+        System.out.println("car3.model : " + car3.model);
+        System.out.println("car3.color : " + car3.color);
+        System.out.println("------------------------------");
+
+        Car car4 = new Car("자가용", "빨강", 200);
+        System.out.println("car4.company : " + car4.company);
+        System.out.println("car4.model : " + car4.model);
+        System.out.println("car4.color : " + car4.color);
+        System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+        System.out.println("------------------------------");
     }
 }
