ac9bdfa144d161bb499dc87ef73376df581eb4f7
장찬영 <jang960222@naver.com>
2025-03-10 18:56:28 +0900

    return 호출 및 메소드 호출

---
+++ src/Class/CarExample3.java
@@ -0,0 +1,11 @@
+package Class;
+
+public class CarExample3 {
+    public static void main(String[] args) {
+        Car3 myCar = new Car3();
+        myCar.keyTurnOn();
+        myCar.run();
+        int speed = myCar.getSpeed();
+        System.out.println("현재 속도 : " + speed + "km/h");
+    }
+}
