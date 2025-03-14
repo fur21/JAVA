71c3483363b76e9e04af541c3ba5b9c36c9d7720
장찬영 <jang960222@naver.com>
2025-03-14 18:12:11 +0900

    인스턴스 멤버와 this

---
+++ src/Class/CarExample4.java
@@ -0,0 +1,11 @@
+package Class;
+
+public class CarExample4 {
+    public static void main(String[] args) {
+        Car4 myCar = new Car4("포르쉐");
+        Car4 yourCar = new Car4("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
