fcf951ba5684d89bb87e594390d856e3bf04d3a0
장찬영 <jang960222@naver.com>
2025-03-28 19:20:02 +0900

    부모 생성자 호출

---
+++ src/Inheritance/StudentExample.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student student = new Student("홍길동", "1234-5678", 1);
+        System.out.println("name : " + student.name);
+        System.out.println("ssn : " + student.ssn);
+        System.out.println("studentNo : " + student.studentNo);
+        System.out.println(student.getClass());
+    }
+}
